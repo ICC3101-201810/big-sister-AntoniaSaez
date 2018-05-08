fda0c3adc0fc5e3d2f3182f7af2a5609d31c5c77
jatellezs <jatellez@miuandes.cl>
2018-05-08 17:50:37 -0300

    Lab

--- LabPOO/Program.cs
+++ LabPOO/Program.cs
@@ -7,13 +7,19 @@ using System.Threading.Tasks;
 
 namespace LabPOO
 {
+    
     class Program
     {
         public static List<Product> cart;
         public static List<Product> market;
+        public static List<Product> ingredientesreceta;
+               
+
 
         static void Main(string[] args)
         {
+            
+
             cart = new List<Product>();
             market = new List<Product>();
             SupplyStore();
@@ -118,6 +124,7 @@ namespace LabPOO
             }
         }
 
+       
         public static void PrintProduct(int index, Product product)
         {
             Console.WriteLine(String.Format("{0}. {1}\n\tPrecio: ${2}\t{3}\tStock: {4}\n", index.ToString(), product.Name, product.Price, product.Unit, product.Stock));
@@ -129,6 +136,8 @@ namespace LabPOO
             Console.WriteLine("\t\t LIDER\n");
         }
 
+        //Agregar al carrito
+
         public static bool AddToCart(Product product)
         {
             return product.Agregar(cart);
@@ -136,29 +145,46 @@ namespace LabPOO
 
         public static void SupplyStore()
         {
-            market.Add(new Product("Leche Entera", 820, 89, "1L"));
+            market.Add(new Product("Leche Entera", 820, 89, "1L")); //ingrediente receta
             market.Add(new Product("Gomitas Flipy", 720, 12, "100g"));
-            market.Add(new Product("Mantequilla", 850, 12, "125g"));
+            market.Add(new Product("Mantequilla", 850, 12, "125g")); //ingrediente receta
             market.Add(new Product("Crema para hemorroides", 4990, 7, "300cc"));
-            market.Add(new Product("Pimienta", 430, 84, "15g"));
-            market.Add(new Product("Vino Sauvignon Blanc Reserva Botella", 4150, 23, "750cc"));
-            market.Add(new Product("Sal Lobos", 330, 150, "1kg"));
+            market.Add(new Product("Pimienta", 430, 84, "15g")); //ingrediente receta
+            market.Add(new Product("Vino Sauvignon Blanc Reserva Botella", 4150, 23, "750cc")); //ingrediente receta
+            market.Add(new Product("Sal Lobos", 330, 150, "1kg")); //ingrediente receta
             market.Add(new Product("Cuaderno Mi Pequeño Pony", 1290, 50, "1un"));
-            market.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));
+            market.Add(new Product("Láminas de Lasaña", 1250, 85, "400g")); //ingrediente receta
             market.Add(new Product("Tomate", 1290, 200, "1kg"));
-            market.Add(new Product("Harina", 890, 43, "1kg"));
+            market.Add(new Product("Harina", 890, 43, "1kg")); //ingrediente receta
             market.Add(new Product("Audifonos Samsung", 5990, 40, "1un"));
             market.Add(new Product("Pisco Alto del Carmen", 5990, 120, "1L"));
-            market.Add(new Product("Carne Molida", 4390, 15, "500g"));
-            market.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));
+            market.Add(new Product("Carne Molida", 4390, 15, "500g")); // ingrediente receta
+            market.Add(new Product("Aceite de Oliva", 1790, 77, "250g")); //ingrediente receta
             market.Add(new Product("Sal parrillera", 840, 50, "750g"));
             market.Add(new Product("Cable HDMI 1m", 3990, 25, "1un"));
             market.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));
             market.Add(new Product("Vino Blanco Caja", 2790, 84, "2L"));
-            market.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));
-            market.Add(new Product("Tomates Pelados en lata", 700, 48, "540g"));
-            market.Add(new Product("Queso Parmesano", 3790, 41, "200g"));
-            market.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));
+            market.Add(new Product("Malla de Cebollas", 1090, 91, "1kg")); //ingrediente receta
+            market.Add(new Product("Tomates Pelados en lata", 700, 48, "540g")); //ingrediente receta
+            market.Add(new Product("Queso Parmesano", 3790, 41, "200g")); //ingrediente receta
+            market.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un")); //ingrediente receta
+        }
+        public static void Ingredientesreceta()
+        {
+            ingredientesreceta.Add(new Product("Leche Entera", 820, 89, "1L"));
+            ingredientesreceta.Add(new Product("Mantequilla", 850, 12, "125g"));
+            ingredientesreceta.Add(new Product("Pimienta", 430, 84, "15g"));
+            ingredientesreceta.Add(new Product("Vino Sauvignon Blanc Reserva Botella", 4150, 23, "750cc"));
+            ingredientesreceta.Add(new Product("Sal Lobos", 330, 150, "1kg"));
+            ingredientesreceta.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));
+            ingredientesreceta.Add(new Product("Harina", 890, 43, "1kg"));
+            ingredientesreceta.Add(new Product("Carne Molida", 4390, 15, "500g"));
+            ingredientesreceta.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));
+            ingredientesreceta.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));
+            ingredientesreceta.Add(new Product("Tomates Pelados en lata", 700, 48, "540g"));
+            ingredientesreceta.Add(new Product("Queso Parmesano", 3790, 41, "200g"));
+            ingredientesreceta.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));
+
         }
 
         public static void ShowRecipe()
---
+++ LabPOO/RevisarCarrito.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabPOO
+{
+    public delegate void RevisarCompraNecesariaDelegate(Product p);
+    
+
+
+    public class RevisarCarrito
+    {
+        public event RevisarCompraNecesariaDelegate QuitarProducto;
+        
+
+        public RevisarCarrito()
+        {
+            QuitarProducto += new RevisarCompraNecesariaDelegate(Retar);
+        }
+        public void ChequearCompra(RevisarCompraNecesariaDelegate deleganterevisarcompra)
+        {
+                    
+            foreach (Product p in Program.cart)
+            {
+                
+                if (Program.ingredientesreceta.Contains(p))
+                {
+                    continue;
+                }
+                else
+                {
+
+                    QuitarProducto(p);
+                    
+                    continue;
+
+                }
+               
+            }
+            
+                        
+
+        }
+        public void Retar(Product p)
+        {
+            Program.cart.Remove(p);
+            Console.WriteLine("Se removio el producto" + p);
+        }
+
+    }
+    
+}
